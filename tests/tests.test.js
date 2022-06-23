17c1d2933d2ab2d0e1cae3a90f75563e04963928
Vinícius Morais <vinicius@cysrc.com>
2022-06-23 15:18:02 -0300

    add test coverage

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,5 +1,8 @@
 const request = require("supertest");
 const app = require("../src/app");
+const covenantBarCodeValidation = require("../src/services/validators/covenantBarCodeValidation");
+const verifierDigitValidation = require("../src/services/validators/verifierDigitValidation");
+const covenantBarCodeServiceValidation = require("../src/services/covenantService/barCodeService");
 
 describe("Boleto de convênio - Casos de sucesso.", () => {
   it("Contém todas as informações.", async () => {
@@ -89,22 +92,55 @@ describe("Boleto de título - Caso de sucesso.", () => {
   });
 });
 
-describe('Boleto de título - Caso de erro.', () => {
-    it('Caracter(es) inválido(s).', async () => {
-        const res = await request(app).get('/boleto/21290001192110001210904475617405975870000E02000');
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('error');
-    });
+describe("Boleto de título - Caso de erro.", () => {
+  it("Caracter(es) inválido(s).", async () => {
+    const res = await request(app).get(
+      "/boleto/21290001192110001210904475617405975870000E02000"
+    );
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+  });
 
-    it('String numérica, porém inválida.', async () => {
-        const res = await request(app).get('/boleto/21290001192110001210904475617405975870000E02000');
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('error');
-    });
+  it("String numérica, porém inválida.", async () => {
+    const res = await request(app).get(
+      "/boleto/21290001192110001210904475617405975870000E02000"
+    );
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+  });
 
-    it('Quantidade de dígitos inválida.', async () => {
-        const res = await request(app).get('/boleto/21290001192110001210904475617405975870000E02000');
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('error');
-    });
+  it("Quantidade de dígitos inválida.", async () => {
+    const res = await request(app).get(
+      "/boleto/21290001192110001210904475617405975870000E02000"
+    );
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+  });
 });
+
+test("Checa a string recebida e a valida.", () => {
+  expect(
+    covenantBarCodeValidation("21290001192110001210904475617405975870000002000")
+  ).toBeTruthy();
+});
+
+test("Checa se o valor retornado é string.", () => {
+  expect(
+    typeof covenantBarCodeValidation(
+      "21290001192110001210904475617405975870000002000"
+    )
+  ).toBe("string");
+});
+
+test("Checa se a função retorna um valor booleano, dependendo da substring após quebra pelos serviços de convênio ou título", () => {
+  expect(typeof verifierDigitValidation("2129000119", "1")).toBe("boolean");
+});
+
+test("Checa se o valor retornado é booleano.", () => {
+  expect(
+    typeof covenantBarCodeServiceValidation(
+      "21290001192110001210904475617405975870000002000"
+    )
+  ).toBe("boolean");
+});
+
